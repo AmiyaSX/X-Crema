09e7d5c4ea1689e7119b2caaa4689c58fd2412e7
sunxi <sunxi@highlight.mobi>
2022-09-22 17:12:38 +0800

    feat: 权限申请逻辑+fragment跳转nav

--- app/src/main/java/com/sxx/x_crema/CameraFragment.kt
+++ app/src/main/java/com/sxx/x_crema/CameraFragment.kt
@@ -1,48 +1,62 @@
 package com.sxx.x_crema
 
+import android.Manifest
+import android.app.Activity
+import android.app.Dialog
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultCaller
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.registerForActivityResult
+import androidx.core.content.ContextCompat
+import androidx.navigation.NavArgs
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
 import com.sxx.x_crema.databinding.FragmentCameraBinding
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
 class CameraFragment : Fragment() {
 
     private var _binding: FragmentCameraBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private val navController: NavController by lazy {
+        Navigation.findNavController(requireActivity(), R.id.fragment_container)
+    }
+
+    private val cameraManager: CameraManager by lazy {
+        requireContext().applicationContext
+            .getSystemService(Context.CAMERA_SERVICE) as CameraManager
+    }
+
+    private val dialog: Dialog by lazy { Dialog(requireContext()) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
+    ): View {
         _binding = FragmentCameraBinding.inflate(inflater, container, false)
         return binding.root
-        binding.cameraBtn.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-//        binding.buttonFirst.setOnClickListener {
-//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-//        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }
--- app/src/main/java/com/sxx/x_crema/MainActivity.kt
+++ app/src/main/java/com/sxx/x_crema/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.sxx.x_crema
 
 import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.SensorPrivacyManager
 import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.CameraDevice
 import android.os.Bundle
@@ -14,55 +16,87 @@ import android.view.Menu
 import android.view.MenuItem
 import com.sxx.x_crema.databinding.ActivityMainBinding
 import android.hardware.camera2.CameraManager
+import android.os.Build
 import android.os.Handler
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import java.util.jar.Manifest
+
+private const val TAG = "MainActivity"
+
+private const val PERMISSIONS_REQUEST_CODE = 10
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-    private lateinit var cameraManager: CameraManager
-    private lateinit var cameraId: String
-    private lateinit var cameraCharacteristics: CameraCharacteristics
-    private lateinit var stateCallback: CameraDevice.StateCallback
+
+    private val navController: NavController by lazy {
+        Navigation.findNavController(this, R.id.fragment_container)
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            isGranted: Boolean ->
+        if (isGranted) {
+            navigateToCamera()
+        } else {
+            Toast.makeText(this, "相机的正常使用需要开启相机权限，请自行前往设置开启权限。", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        checkCameraPermission()
+        checkDevice()
         setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        cameraManager = this.getSystemService(Context.CAMERA_SERVICE) as CameraManager  //获取CameraManager实例
-        cameraId = cameraManager.cameraIdList[0]
-        cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraId)
-
-
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+    //相机权限监测和申请
+    private fun checkCameraPermission() {
+        when {
+            ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+                    == PackageManager.PERMISSION_GRANTED -> navigateToCamera()
+            else -> {
+                requestPermissionLauncher.launch(android.Manifest.permission.CAMERA)
+            }
+        }
     }
 
+    //检查设备是否支持麦克风和摄像头切换开关
+    private fun checkDevice() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                val sensorPrivacyManager =  applicationContext
+                    .getSystemService(SensorPrivacyManager::class.java)
+                        as SensorPrivacyManager
+                val supportsMicrophoneToggle = sensorPrivacyManager
+                    .supportsSensorToggle(SensorPrivacyManager.Sensors.MICROPHONE)
+                val supportsCameraToggle = sensorPrivacyManager
+                    .supportsSensorToggle(SensorPrivacyManager.Sensors.CAMERA)
+            } else {
+
+            }
+        }
+
+    }
+    private fun navigateToCamera() {
+        navController.navigate(R.id.action_Homeragment_to_CameraFragment)
+    }
 }
